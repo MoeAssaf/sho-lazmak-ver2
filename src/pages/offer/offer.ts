7ec34321fd136bf25d70ebb3072db24d9a9fc583
Moe Assaf <mohammadassaf160@gmail.com>
2018-07-01 13:14:35 +0300

    ability to add product

--- src/pages/offer/offer.ts
+++ src/pages/offer/offer.ts
@@ -13,6 +13,7 @@ import  { FirebaseApp } from 'angularfire2';
 import{ Profile } from '../../models/details'
 import { Firebase } from '@ionic-native/firebase';
 import { Storage } from '@ionic/storage';
+import { ProfilePage } from '../settings/profile/profile';
 /**
  * Generated class for the OfferPage page.
  *
@@ -62,6 +63,7 @@ export class Product {
   email: any
   profile: any
   ready: boolean
+  image: any
   product= {} as Product_details
   constructor
   ( public navCtrl: NavController,
@@ -99,13 +101,100 @@ dismiss() {
   this.viewCtrl.dismiss();
 }
 logForm(){
-
+  if(this.product.name == null || this.product.name == ""||
+  this.product.info == null || this.product.info == ""||
+  this.product.price == null ||
+  this.product.category == null || this.product.category == ""||
+  this.product.state == null || this.product.state == ""){
+    console.log(this.product)
+    this.alert('Please provide all information, all fields are required!')
+  }
+  else if(this.image == null){
+    this.alert('Please add a picture by tapping the grey space!')
+  }
+  else{
+    this.addProduct()
+  }
 }
 addProduct(){
+  this.AFauth.authState.take(1).subscribe(auth =>{
+  this.afDB.list(`public_product/${this.product.category}/${auth.uid}`).push(this.product).then((data) => {
+    this.uploadPhoto(`public_product/${auth.uid}/${data.key}/image`)
 
+} )
+  }
+)
 }
 openForm(){
-  
+  this.navCtrl.push(ProfilePage)
+}
+presentActionSheet() {
+  let actionSheet = this.actionSheetCtrl.create({
+    title: 'Set profile picture',
+    buttons: [
+      // {
+      //   text: 'Destructive',
+      //   role: 'destructive',
+      //   handler: () => {
+      //     console.log('Destructive clicked');
+      //   }
+      // },
+      {
+        text: 'Take photo',
+        handler: () => {
+          this.addPhoto();
+          console.log('Archive clicked');
+        }
+      },
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }
+    ]
+  });
+
+  actionSheet.present();
+}
+async addPhoto(){
+  try {
+  const options: CameraOptions = {
+    quality: 50,
+    targetHeight: 600,
+    targetWidth: 600,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE,
+    correctOrientation: true
+  };
+  const result = await this.camera.getPicture(options);
+
+  this.image = `data:image/jpeg;base64,${result}`;
+
+}
+catch(e){
+  console.log(e)
+}
+}
+async uploadPhoto(path){
+try{
+const pictures = storage().ref(path);
+pictures.putString(this.image, 'data_url');
+this.navCtrl.setRoot(OfferPage)
+}
+catch(e){
+this.alert('An error occured please try again!')}
+}
+alert(comment){
+  console.log(comment);
+  let ALERT = this.alertCtrl.create({
+    title:"Alert",
+    message: comment,
+    buttons:[{text: " OK!"}]
+  })
+  ALERT.present()
 }
 }
 
